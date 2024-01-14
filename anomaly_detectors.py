99e3941cd4608a11a59b8bd2b88f5ce233978fdf
evo_d21672 <evo_d21672@outlook.de>
2024-01-14 21:30:01 +0100

    Added some ideas/notes that we generated a few weeks ago

--- anomaly_detectors.py
+++ anomaly_detectors.py
@@ -1,5 +1,14 @@
 import pandas as pd
 
+# Ideas so far:
+# a) activation, request Signal:    derivative, to detect opposite direction (asynchronous)
+# b) correction, correctionError:   Freeze of at least echo signal with simple threshold (window size) and derivative 
+# c) correction, correctionError:   cross-correlation in window, to detect larger delay of 8 seconds
+# d) FRCE, LFCInput:                According to "Anomaly 3" example that the signal FRCE gets a "harten" if signal LFCInput has a "higher"
+#                                   positive peak. However, it seems only the part until FRCE falls down and intersects with LFCInput again
+#                                   is counted as anomaly
+
+
 
 def detect_freeze(dataset, signalname: str, min_duration: float, threshold: float = 0.001) -> pd.Series:
     """
